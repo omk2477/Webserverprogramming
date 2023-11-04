25040bab90eebe02a9b50071ae8b7c3f8f9a24d0
omk2477 <omk2477@gmail.com>
2023-11-04 16:03:44 +0900

    add: 10주차 마이페이지추가

--- week2/web/routes/users.js
+++ week2/web/routes/users.js
@@ -16,4 +16,9 @@ router.get('/join', function(req, res, next) {
   res.render('index', { title: '회원가입', pageName: 'users/join.ejs' });
 });
 
+// 마이 페이지
+router.get('/mypage', function(req, res, next) {
+  res.render('index', { title: '마이페이지', pageName: 'users/mypage.ejs' });
+});
+
 module.exports = router;
